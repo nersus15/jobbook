17084f68392e254ee530c1cbf80194e8f6aa0a88
fathurrahman <fathur.ashter15@gmail.com>
2020-01-20 20:31:08 +0800

    add function __construct() in my.php

--- apps/controller/my.php
+++ apps/controller/my.php
@@ -1,6 +1,12 @@
 <?php
 class my extends controller
 {
+    function __construct()
+    {
+        if (!isset($_SESSION['user_data'])) {
+            header("Location: " . BASEURL);
+        }
+    }
     function index()
     {
         echo "Login";
