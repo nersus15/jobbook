7f6d1f5dffc2694ef853de2f93fd03655a7f02c9
fathurrahman <fathur.ashter15@gmail.com>
2020-01-21 01:59:07 +0800

    update sidebar

--- apps/views/navigasi/filter_sidebar.php
+++ apps/views/navigasi/filter_sidebar.php
@@ -1,18 +1,37 @@
 <style>
     .sidebar-wrapper {
         margin-left: 3%;
-        width: 7%;
+        width: 20%;
         position: fixed;
         height: 100%;
         left: 0;
     }
 </style>
-<div class="sidebar-wrapper" style=" width:25%; height:100%">
-
-
+<div class="sidebar-wrapper">
+    <h4 style="color: grey">Filter Pekerjaan</h4>
     <form action="">
-        <input class="form-control" type="text" name="" id="">
-        <input class="form-control" type="text" name="" id="">
+        <div class="form-group">
+            <label for="kategori">Kategori</label>
+            <select class="custom-select form-control" name="kategor" id="kategori">
+                <option value="">kategori1</option>
+                <option value="">kategori2</option>
+                <option value="">kategori3</option>
+                <option value="">kategori4</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <select class="custom-select form-control" name="" id=""></select>
+        </div>
+        <div class="form-group">
+            <label for="formControlRange">Bayaran Pekerjaan</label>
+            <input min="0" value="500000" oninput="nilai(value)" max="1000000" type="range" class="form-control-range" id="formControlRange">
+            <output for="vol" id="volume">Rp.500000</output>
+        </div>
+        <script>
+            function nilai(vol) {
+                document.querySelector('#volume').value = "Rp." + vol;
+            }
+        </script>
     </form>
 
 
