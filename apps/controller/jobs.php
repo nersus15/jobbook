9c0931eee3f9167897dad177a1c0bf85cd0ae562
fathurrahman <fathur.ashter15@gmail.com>
2020-01-21 01:23:08 +0800

    add sidebar and navbar

--- apps/controller/jobs.php
+++ apps/controller/jobs.php
@@ -5,6 +5,8 @@ class jobs extends controller
     {
         $data['page_title'] = "Jobbook";
         $this->view('head/main', $data);
+        $this->view('navigasi/main_navbar');
+        $this->view('navigasi/filter_sidebar');
         $this->view('pekerjaaan/tpekerjaan', $data);
         $this->view('foot/main', $data);
     }
---
+++ apps/views/navigasi/filter_sidebar.php
@@ -0,0 +1,20 @@
+<style>
+    .sidebar-wrapper {
+        margin-left: 3%;
+        width: 7%;
+        position: fixed;
+        height: 100%;
+        left: 0;
+    }
+</style>
+<div class="sidebar-wrapper" style=" width:25%; height:100%">
+
+
+    <form action="">
+        <input class="form-control" type="text" name="" id="">
+        <input class="form-control" type="text" name="" id="">
+    </form>
+
+
+
+</div>
