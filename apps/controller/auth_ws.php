2650e8c6c04d2aeaa76a00c2bb61ea8ef28cd9f9
fathurrahman <fathur.ashter15@gmail.com>
2020-01-20 20:29:59 +0800

    add register method in auth_ws.php

--- apps/controller/auth_ws.php
+++ apps/controller/auth_ws.php
@@ -20,5 +20,43 @@ class auth_ws extends controller
     }
     function register()
     {
+        $data = $_POST;
+        // Persiapan
+        $params = array("username" => $data['username'], "email" => $data['email'], "no_hp" => $data['no_hp']);
+        $accounts = $this->model('User_model')->getUser($params);
+        // Logic untuk email boleh kosong
+        $data['email'] = ($data['email'] == null) ? '-' : $data['email'] = $data['email'];
+
+
+        // Logic untuk cek inputan tidak boleh sama
+        foreach ($accounts as $account) {
+            if ($data['email'] != "-" and $data['email'] == $account['email']) {
+                flasher::setFlash("Gagal, Email sudah digunakan", "danger");
+                header("Location: " . BASEURL . "/user/register");
+            } else if ($data['no_hp'] == $account['no_hp']) {
+                flasher::setFlash("Gagal, No Hp sudah digunakan", "danger");
+                header("Location: " . BASEURL . "/user/register");
+            } elseif ($data['username'] == $account['username']) {
+                flasher::setFlash("Gagal, Username sudah digunakan", "danger");
+                header("Location: " . BASEURL . "/user/register");
+            }
+        }
+        // Logic untuk menyamakan password
+        if ($data['password'] != $data['cpassword']) {
+            flasher::setFlash('Gagal, Password tidak sama', 'danger');
+            header('Location: ' . BASEURL . 'user/register');
+        }
+        // more logic
+        # code ...
+
+        // post data
+        $this->model('User_model')->register($data);
+        flasher::setFlash("Berhasil, Akun anda sudah dibuat", "success");
+        header('Location: ' . BASEURL . 'user/register');
+    }
+    function logout()
+    {
+        unset($_SESSION['user_data']);
+        header('location:' . BASEURL);
     }
 }
